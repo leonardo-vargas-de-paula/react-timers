9ce98f74b5b23c1e355331488d78dca099a81b0c
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-06 07:26:41 -0400

    refactor: remove elementos do componente TimerForm

--- src/component/TimerForm/index.jsx
+++ src/component/TimerForm/index.jsx
@@ -3,7 +3,7 @@ import styles from  "./TimerForm.module.css"
 function TimerForm() {
     return(
         <form action="">
-            <h1 className={styles.title}>Form</h1>
+            
         </form>
     );
 }
