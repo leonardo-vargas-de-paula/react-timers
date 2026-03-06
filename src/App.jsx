6e19248f3e6fca9b705886cec634c9ae894ffccf
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-06 07:14:36 -0400

    feat: add timerform no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import TimerForm from './component/TimerForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <main>
+      <h1>Timer</h1>
+      <TimerForm />
+    </main>
   )
 }
 
