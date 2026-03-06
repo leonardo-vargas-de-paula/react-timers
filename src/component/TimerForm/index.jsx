45e77ba6f25e50f29c8ca712db6da525f01ea8b1
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-06 08:25:43 -0400

    feat: adiciona inputs

--- src/component/TimerForm/index.jsx
+++ src/component/TimerForm/index.jsx
@@ -1,9 +1,13 @@
-import styles from  "./TimerForm.module.css"
+import styles from "./TimerForm.module.css"
 
 function TimerForm() {
-    return(
-        <form action="">
-            
+    return (
+        <form action="" className={styles.form}>
+            <input type="text" name="name" placeholder="Nome"
+                className={styles.input} />
+            <input type="number" name="duration" placeholder="Duração (segundos)"
+                className={styles.input} min="1"/>
+            <input type="submit" value="+" className={styles.submitButton}/>
         </form>
     );
 }
