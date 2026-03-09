617c65215f274065c8c336ecefcd62459b8d2fd7
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-09 08:26:35 -0400

    feat: passa o setTimers para o TimerForm

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import styles from './App.module.css'
 import TimerForm from './components/TimerForm'
 
 function App() {
-  const [timers, setTimers] = useState([
-    {name: "teste1", duration: 3},
-    {name: "teste2", duration: 5}
-  ])
+  const [timers, setTimers] = useState([])
 
   return (
     <main className={styles.main}> 
       <h1 className={styles.title}>Timer</h1>
-      <TimerForm />
+      <TimerForm setTimer={setTimers}/>
       <div className={styles.timers}>
         {timers.map((timer)=>(
-          <h3>{timer.name}</h3>
+          <h3 key={timer.id}>{timer.name}</h3>
         ))}
       </div>
     </main>
