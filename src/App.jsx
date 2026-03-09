20a5fb573d9432dfbd940f656b4eedebe6c6cf3c
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-09 08:14:59 -0400

    feat: add timers com exemplo de teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import styles from './App.module.css'
 import TimerForm from './components/TimerForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timers, setTimers] = useState([
+    {name: "teste1", duration: 3},
+    {name: "teste2", duration: 5}
+  ])
 
   return (
     <main className={styles.main}> 
       <h1 className={styles.title}>Timer</h1>
       <TimerForm />
+      <div className={styles.timers}>
+        {timers.map((timer)=>(
+          <h3>{timer.name}</h3>
+        ))}
+      </div>
     </main>
   )
 }
